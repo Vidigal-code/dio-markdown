24f6650fd64969e8ac9a356c01bb5dfc2b90d7f3
Kauan Vidigal <kauan.vidigal@gmail.com>
2024-06-04 00:35:42 -0300

    update git pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Importe createRoot de 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = createRoot(document.getElementById('root')); // Use createRoot para criar a raiz da aplicação
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('Element with "root" id not found.');
+}
